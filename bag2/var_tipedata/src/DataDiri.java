f52c6de747615ccc65f420927a7eec7926768a18
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2025-07-14 13:02:38 +0700

    up bag2 variabel dan tipe data

---
+++ bag2/var_tipedata/src/DataDiri.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class DataDiri {
+    public static void main(String[] args) {
+        //membuat variabel
+        String nama, alamat;
+        int usia;
+        double tinggi;
+
+        //mengisi variabel
+        nama = "vermilion10";
+        alamat = "internet";
+        usia = 14;
+        tinggi = 160.0;
+
+        //mencetak ke layar isi varibale
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("tinggi: " + tinggi + " cm");
+    }
+}
---
+++ bag2/var_tipedata/src/LuasLingkaran.java
@@ -0,0 +1,17 @@
+public class LuasLingkaran {
+    public static void main(String[] args){
+        //deklarasi
+        double luas, pi;
+        int r;
+
+        //input
+        pi = 3.14;
+        r = 18;
+
+        //proses
+        luas = pi * r * r;
+
+        //output
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
