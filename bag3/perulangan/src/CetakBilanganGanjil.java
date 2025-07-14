d03417e6fe1db6691c81f21abf490d926481da27
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2025-07-14 21:29:53 +0700

    up loop lesson

---
+++ bag3/perulangan/src/CetakBilanganGanjil.java
@@ -0,0 +1,9 @@
+class CetakBilanganGanjil{
+
+    public static void main(String[] args){
+        for(int i = 1; i <= 20; i += 2){
+            System.out.print( i + " ");
+        }
+    }
+
+}
---
+++ bag3/perulangan/src/PerulanganForeach.java
@@ -0,0 +1,11 @@
+public class PerulanganForeach {
+    public static void main(String[] args){
+        //mebuat ary
+        int angka[] = {3,1,42,24,12};
+
+        //menggunakan perulangan for each untuk m/tampil angka
+        for(int x : angka ){
+            System.out.print(x + " ");
+        }
+    }
+}
